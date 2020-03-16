5b28d8d41338372ff4e3d615a7f7ebdf65197603
Jose Laercio <joselaerciorlima@hotmail.com>
2020-03-16 16:04:48 -0300

    Create class
    Create class ActivityModel, ProductModel,SettingDataBaseModel

---
+++ Model/ProductModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Model
+{
+   /// <summary>
+   /// Classe responsável por criar o objeto produto.
+   /// </summary>
+   public class ProductModel
+   {
+      public int codproduct { get; set; } //Código do produto
+      public string typeproduct { get; set; } //Tipo do produto
+      public string modelproduct { get; set; } //Modelo do produto
+      public string providerproduct { get; set; } //fornecedor do produto (marca)
+      public string sizeproduct { get; set; } //Tamanho do produto
+      public decimal amountproduct { get; set; } //Quantidade do produto
+      public string storageproduct { get; set; } //Localização do produto
+      public string imageproduct { get; set; } //Endereço local da imagem
+      public int statusproduct { get; set; } //Status do produto (ativo/inativo)
+      public string logproduct { get; set; } //Armazena todos as alterações referente ao produto (data, hora e usuário)
+
+   }
+}
