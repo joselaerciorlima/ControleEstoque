8f8a87f87606fe63d4b0a4ba56874965e5978fbd
Jose Laercio <joselaerciorlima@hotmail.com>
2020-03-16 16:45:08 -0300

    Create class
    Create class ConnectionDataBaseController and ProductController

---
+++ Controller/ConnectionDataBaseController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Model;
+
+namespace Controller
+{
+   public class ConnectionDataBaseController
+   {
+      /// <summary>
+      /// Carrega as configurações do banco de dados armazenadas no arquivo de configuração. Essas informações são passadas através de variáveis.
+      /// </summary>
+      string parameters = string.Format(@"Data Source={0};Initial Catalog={1};User ID={2};Password={3}",
+          Properties.Settings.Default.server,            //Endereço do servidor
+          Properties.Settings.Default.dataBase,          //Nome do banco de dados
+          Properties.Settings.Default.userDataBase,      //Usuário do banco de dados
+          Properties.Settings.Default.passDataBase);     //Senha do banco de dados
+
+      SqlConnection connection = null;
+
+      /// <summary>
+      /// Método responsável por carregar as informações salvas no objeto de configuração do banco de dados para dentro das variáveis do arquivo de configuração.
+      /// </summary>
+      public SettingsDataBaseModel LoadSettings()
+      {
+         SettingsDataBaseModel settingsDataBase = new SettingsDataBaseModel();
+         settingsDataBase.Server = Properties.Settings.Default.server;
+         settingsDataBase.DataBase = Properties.Settings.Default.dataBase;
+         settingsDataBase.UserDataBase = Properties.Settings.Default.userDataBase;
+         settingsDataBase.PassDataBase = Properties.Settings.Default.passDataBase;
+
+         return settingsDataBase;
+      }
+
+      /// <summary>
+      /// Método responsável por salvar os valores do objeto de configuração do banco de dados dentro do arquivo de configuração mesmo após o projeto ter sido compilado.
+      /// </summary>
+      public bool SaveSettings(SettingsDataBaseModel settingsDataBase)
+      {
+         try
+         {
+            Properties.Settings.Default.server = settingsDataBase.Server;
+            Properties.Settings.Default.dataBase = settingsDataBase.DataBase;
+            Properties.Settings.Default.userDataBase = settingsDataBase.UserDataBase;
+            Properties.Settings.Default.passDataBase = settingsDataBase.PassDataBase;
+
+            Properties.Settings.Default.Save();
+
+            return true;
+         }
+         catch (Exception error)
+         {
+            throw error; //Em caso de erro, mostra a mensagem para quem o chamou.
+         }
+      }
+      /// <summary>
+      /// Método responsável por abrir a conexão com o banco de dados.
+      /// </summary>
+      public SqlConnection Conect()
+      {
+         try
+         {
+            connection = new SqlConnection(parameters);
+            if (connection.State == ConnectionState.Closed)
+            {
+               connection.Open();
+            }
+
+            return connection;
+
+         }
+         catch (Exception error)
+         {
+            connection.Close();
+            throw error;
+         }
+      }
+
+      /// <summary>
+      /// Método responsável por fechar a conexão com o banco de dados.
+      /// </summary>
+      public void Disconect()
+      {
+         connection.Close();
+      }
+   }
+}
---
+++ Controller/ProductController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Model;
+
+namespace Controller
+{
+  public class ProductController
+   {
+		/// <summary>
+		/// Método responsável por salvar os valores dentro do objeto Produto no banco de dados para cadastrar.
+		/// </summary>
+      public int Register(ProductModel product)
+      {
+			try
+			{
+				return 1;
+			}
+			catch (Exception error)
+			{
+
+				throw error;
+			}
+			finally
+			{
+
+			}
+      }
+
+		/// <summary>
+		/// Método responsável por salvar os valores dentro do objeto Produto no banco de dados para atualizar de acordo com o código.
+		/// </summary>
+		public int Modify(ProductModel product)
+		{
+			try
+			{
+				return 1;
+			}
+			catch (Exception error)
+			{
+
+				throw error;
+			}
+			finally
+			{
+
+			}
+		}
+
+		/// <summary>
+		/// Método responsável por fazer uma busca no banco de dados de acordo com o filtro ativado.
+		/// </summary>
+		public DataTable Search(string filter, string filterType)
+		{
+			try
+			{
+				DataTable dt = new DataTable();
+				return dt;
+			}
+			catch (Exception error)
+			{
+
+				throw error;
+			}
+			finally
+			{
+
+			}
+		}
+
+		/// <summary>
+		/// Método responsável por deletar o registro no banco de dados de acordo com o código.
+		/// </summary>
+		public int Remove(int codproduct)
+		{
+			try
+			{
+				return 1;
+			}
+			catch (Exception error)
+			{
+
+				throw error;
+			}
+			finally
+			{
+
+			}
+		}
+	}
+}
